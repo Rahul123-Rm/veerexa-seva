a523a2804a807f2022f298c06a32eabeba65d54d
Rahul Meena <rahulmeena4767@example.com>
2026-08-21 00:36:48 +0530

    Added SEO

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,46 +1,54 @@
-<?xml version="1.0" encoding="UTF-8"?>
+import { writeFileSync } from 'fs';
+
+// Auto get today's date in YYYY-MM-DD format
+const today = new Date().toISOString().split('T')[0];
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
   <url>
     <loc>https://kiosk-software.veerexa.com/</loc>
-    <lastmod>2026-08-20</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
 
   <url>
     <loc>https://kiosk-software.veerexa.com/#software</loc>
-    <lastmod>2026-08-20</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.9</priority>
   </url>
 
   <url>
     <loc>https://kiosk-software.veerexa.com/#ifsc</loc>
-    <lastmod>2026-08-20</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
 
   <url>
     <loc>https://kiosk-software.veerexa.com/#tools</loc>
-    <lastmod>2026-08-20</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>
 
   <url>
     <loc>https://kiosk-software.veerexa.com/#calculators</loc>
-    <lastmod>2026-08-20</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.6</priority>
   </url>
 
   <url>
     <loc>https://kiosk-software.veerexa.com/#schemes</loc>
-    <lastmod>2026-08-20</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.6</priority>
   </url>
 
-</urlset>
+</urlset>`;
+
+writeFileSync('./public/sitemap.xml', sitemap);
+console.log(`✅ Sitemap generated with date: ${today}`);
